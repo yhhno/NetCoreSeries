ae0c8bbfecbbe8746659a7209576c83d942aeb3d
yhhno <516061849@qq.com>
2018-04-13 17:51:52 +0800

    开始分布式项目学习

---
+++ 02ASP.NET Core分布式项目实践/01Identity Server 4 准备/IdentityServerCenter/Config.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;//对应IEnumerable  List
+using IdentityServer4.Models;//对应 ApiResource
+
+namespace IdentityServerCenter
+{
+    /// <summary>
+    /// 初始化identity server
+    /// </summary>
+    public class Config
+    {
+        //这个会用来所有可以访问的对象，也就是用户的资源
+        public static IEnumerable<ApiResource> GetResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api","MY Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            //到最后我们会做一个web界面，把它全部统一添加进去，现在其实它是相当于在内存里来管理我们的clients  为啥内存  因为放在list里面
+            return new List<Client>//也是会返回客户端的枚举
+            {
+                new Client()
+                {
+                    ClientId="client",//clientid
+                    AllowedGrantTypes=GrantTypes.ClientCredentials,//允许访问的方式
+                    ClientSecrets =
+                    {
+                        new Secret("secrect".Sha256())//我们最好对clientsecrect进行简单的加密  Sha256为Identity server的一个扩展方法
+                    },
+                    AllowedScopes={ "api"}// 允许访问的资源
+                }
+            };
+        }
+    }
+}
---
+++ 02ASP.NET Core分布式项目实践/01Identity Server 4 准备/IdentityServerCenter/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+
+
+
+using IdentityServer4;//对应services.AddIdentityServer()
+
+namespace IdentityServerCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //在添加mvc之前 添加 Identitysever
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+
+                //更改Identity server 4配置 
+                .AddInMemoryApiResources(Config.GetResources())
+                .AddInMemoryClients(Config.GetClients());
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //app.UseMvc();//不使用mvc先注释了
+
+            app.UseIdentityServer();//使用中间件
+        }
+    }
+}
