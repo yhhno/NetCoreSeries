942cde29b0968ddcbcacaa83528fbe1b449a628c
yhhno <516061849@qq.com>
2018-04-27 17:39:15 +0800

    task 37

---
+++ 02ASP.NET Core分布式项目实践/02Docker/dockerbuild/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World From Docker!");
+            //Console.ReadLine();
+
+
+            Console.WriteLine("Hello World From Docker!");
+
+            Thread.Sleep(Timeout.Infinite);//县城一直sleep下去
+        }
+    }
+}
